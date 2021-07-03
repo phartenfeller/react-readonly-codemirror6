7917ffa55aa42aa9d324a1d08707bc9a2e7e9155
phartenfeller <pharti@hotmail.de>
2021-07-03 09:06:03 +0200

    restructuring

---
+++ demo/src/App.js
@@ -0,0 +1,19 @@
+import CodeMirror from '../../index';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header className="App-header">
+        <h1 className="header-text">React Codemirror Demo</h1>
+      </header>
+      <main>
+        <CodeMirror>
+          let x = 2;
+        </CodeMirror>
+      </main>
+    </div>
+  );
+}
+
+export default App;
